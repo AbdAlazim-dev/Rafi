8a4ce65f97b11be699deeb115af88192f403ee3e
AbdAlazim-dev <abdalazimattya.dev@gmail.com>
2023-09-17 22:32:37 +0300

    finish poilerplate and add allData

---
+++ Rafi/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1>السلام عليكم ايها العالم</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ Rafi/src/components/AllData.js
@@ -0,0 +1,184 @@
+import apple from "../images/product-images/apple.png";
+import bannana from "../images/product-images/bannana.png";
+import orange from "../images/product-images/orange.png";
+import bagdons from "../images/product-images/bagdons.png";
+import fjl from "../images/product-images/fjl.png";
+import ananas from "../images/product-images/ananas.png";
+import botato from "../images/product-images/botato.png";
+import carrot from "../images/product-images/carrot.png";
+import kazbara from "../images/product-images/kazbara.png";
+import folfol from "../images/product-images/folfol.png";
+import khs from "../images/product-images/khs.png";
+import onion from "../images/product-images/onion.png";
+import tomato from "../images/product-images/tomato.png";
+import shabat from "../images/product-images/shabat.png";
+import mango from "../images/product-images/mango.png";
+import farawla from "../images/product-images/farawla.png";
+import bathnjan from "../images/product-images/bathnjan.png";
+import malfof from "../images/product-images/malfof.png";
+import mint from "../images/product-images/mint.png";
+import wonion from "../images/product-images/white-onion.png";
+
+const data = [
+  {
+    id: 1,
+    name: "تفاح",
+    price: 9.99,
+    image: apple,
+    category: "فواكه",
+    origin: "امريكا",
+  },
+  {
+    id: 2,
+    name: "موز",
+    price: 5.99,
+    image: bannana,
+    category: "فواكه",
+    origin: "امريكا",
+  },
+  {
+    id: 3,
+    name: "برتقال",
+    price: 8.99,
+    image: orange,
+    category: "فواكه",
+    origin: "امريكا",
+  },
+  {
+    id: 4,
+    name: "باذنجان",
+    price: 7.99,
+    image: bathnjan,
+    category: "خضار",
+    origin: "امريكا",
+  },
+  {
+    id: 5,
+    name: "بطاطا",
+    price: 5.99,
+    image: botato,
+    category: "خضار",
+    origin: "امريكا",
+  },
+  {
+    id: 6,
+    name: "جزر",
+    price: 7.99,
+    image: carrot,
+    category: "خضار",
+    origin: "امريكا",
+  },
+  {
+    id: 7,
+    name: "فلفل",
+    price: 10.99,
+    image: folfol,
+    category: "خضار",
+    origin: "امريكا",
+  },
+  {
+    id: 8,
+    name: "فجل",
+    price: 10.99,
+    image: fjl,
+    category: "خضار",
+    origin: "امريكا",
+  },
+  {
+    id: 9,
+    name: "كزبرة",
+    price: 0.99,
+    image: kazbara,
+    category: "خضار",
+    origin: "امريكا",
+  },
+  {
+    id: 10,
+    name: "خس",
+    price: 1.99,
+    image: khs,
+    category: "خضار",
+    origin: "امريكا",
+  },
+  {
+    id: 11,
+    name: "بصل",
+    price: 6.99,
+    image: onion,
+    category: "خضار",
+    origin: "امريكا",
+  },
+  {
+    id: 12,
+    name: "طماطم",
+    price: 7.99,
+    image: tomato,
+    category: "خضار",
+    origin: "امريكا",
+  },
+  {
+    id: 13,
+    name: "شبت",
+    price: 0.99,
+    image: shabat,
+    category: "خضار",
+    origin: "امريكا",
+  },
+  {
+    id: 14,
+    name: "مانجو",
+    price: 28.99,
+    image: mango,
+    category: "فواكه",
+    origin: "امريكا",
+  },
+  {
+    id: 15,
+    name: "فراولة",
+    price: 24.99,
+    image: farawla,
+    category: "فواكه",
+    origin: "امريكا",
+  },
+  {
+    id: 16,
+    name: "ملفوف",
+    price: 8.99,
+    image: malfof,
+    category: "خضار",
+    origin: "امريكا",
+  },
+  {
+    id: 17,
+    name: "نعناع",
+    price: 1,
+    image: mint,
+    category: "خضار",
+    origin: "امريكا",
+  },
+  {
+    id: 18,
+    name: "بصل ابيض",
+    price: 9.99,
+    image: wonion,
+    category: "خضار",
+    origin: "امريكا",
+  },
+  {
+    id: 19,
+    name: "اناناس",
+    price: 12.99,
+    image: ananas,
+    category: "فواكه",
+    origin: "امريكا",
+  },
+  {
+    id: 20,
+    name: "باجدونس",
+    price: 1.99,
+    image: bagdons,
+    category: "خضار",
+    origin: "امريكا",
+  },
+];
+export default data;
